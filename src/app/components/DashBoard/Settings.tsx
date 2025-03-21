47c6eb06f88e6fe0c604eed14ee8e3044a890ab5
Tygrys11 <juskuch2007@gmail.com>
2025-03-21 22:00:57 +0100

    VercelRules

--- src/app/components/DashBoard/Settings.tsx
+++ src/app/components/DashBoard/Settings.tsx
@@ -1,11 +1,9 @@
 "use client"
-import { UserProfile } from "@clerk/clerk-react";
 import styles from "../../styles/OtherPagesStyles/dashBoard.module.css";
 
 export default function Settings() {
     return (
       <div className={styles.dashboard}>
-        {/* <UserProfile /> */}
         <h1>Settings</h1>
       </div>
     );
