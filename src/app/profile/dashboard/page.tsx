119e17b7a4cd1dd23db4d2693bc29f5f390a85d8
Tygrys11 <juskuch2007@gmail.com>
2025-03-26 23:46:07 +0100

    dashboardStyles

--- src/app/profile/dashboard/page.tsx
+++ src/app/profile/dashboard/page.tsx
@@ -1,6 +1,3 @@
-import DashBoard from "@/app/components/DashBoard/DashBoard";
-import DashBoardSidebar from "@/app/components/DashBoard/DashBoardSidebar";
-import styles from "../../../app/styles/OtherPagesStyles/dashBoard.module.css";
 import { UserButton } from "@clerk/nextjs";
 export default function DashboardPage() {
   return (
